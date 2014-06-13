5b4bd45abf89de83b516ad3159530f019886b4c5
Ezra Brooks <ezra.m.brooks@gmail.com>
2014-06-13 19:16:05 -0400

    bam. XHR asking for gfycat querying complete.

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
 //Main background script for JiffyGIF
 function main(details){ //where the magic happens
 	if(details.url.lastIndexOf('.gif') != -1){
+		console.log("It's a GIF!");
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', encodeURIComponent('http://gfycat.com/cajax/checkUrl/' + encodeURI(details.url)), false);
+		xhr.open('GET', 'http://gfycat.com/cajax/checkUrl/' + encodeURIComponent(encodeURI(details.url)), false);
 		xhr.send();
-		
+		console.log(xhr.response);
+		//handle checking here
+		if(xhr.response.urlKnown == true){
+			//go to the gfycat page
+		}else{
+			//upload it to gfycat
+		}
 	}
 }
 chrome.runtime.onInstalled.addListener(function(details){ //DOUBLE EVENT LISTENERS ALL THE WAY ACROSS THE SKY
