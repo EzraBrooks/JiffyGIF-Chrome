1eacee3b151163aa31359b1b67e84294448d5482
Ezra Brooks <ezra.m.brooks@gmail.com>
2014-07-08 22:39:11 -0400

    massive restructuring to use webRequest
    Now uses less API permissions and reliably redirects!

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-/* Main background script for JiffyGIF*|
+/* jiffyMain background script for JiffyGIF*|
 |* Copyright (C) 2014 Ezra Brooks     */
-function main(details){ //where the magic happens
-	if(details.url.lastIndexOf('.gif') + '.gif'.length == details.url.length){
+function jiffyMain(details){ //where the magic happens
+	if(details.type == 'main_frame' && details.url.lastIndexOf('.gif') + '.gif'.length == details.url.length){
 		//console.log('It's a GIF!');
 		var gfyChecker = new XMLHttpRequest();
 		gfyChecker.open('GET', 'http://gfycat.com/cajax/checkUrl/' + encodeURIComponent(encodeURI(details.url)), false);
@@ -10,32 +10,32 @@ function main(details){ //where the magic happens
 		if(gfyChecker.status >= 300){
 			console.log('Contacting gfycat failed. HTTP error ' + gfyChecker.status + '.');
 		}else{
-			chrome.tabs.update(details.tabId, {})
-			//TODO: find some way to pause navigation before even partially loading original .gif
-			if(gfyData.urlKnown == true){ //go to the gfycat page
-				chrome.tabs.update(details.tabId, {url:gfyData.gfyUrl});
+			if(gfyData.urlKnown == true){ //redirect to the gfycat page
+				return {redirectUrl: gfyData.gfyUrl};
 			}else{ //have gfy convert it for us
+				//generate a unique session ID for gfy transcode request
 				var epochTime = Math.round(Date.now() / 1000).toString();
 				var key = epochTime.slice(epochTime.length - 6, epochTime.length);
 				for(var i = 0; i >= 10 - key.length; i++){
 					var randNum = Math.round(Math.random() * 16);
 					key += randNum.toString(16);
 				}
+				//send the request using the session ID
 				var transcodeRequest = new XMLHttpRequest();
 				transcodeRequest.open('GET', 'http://upload.gfycat.com/transcode/' + key + '?fetchUrl=' + encodeURIComponent(encodeURI(details.url)), false);
 				transcodeRequest.send();
 				var gfyResponse = JSON.parse(transcodeRequest.response);
-				chrome.tabs.update(details.tabId, {url:'http://www.gfycat.com/' + gfyResponse.gfyname});
+				return {redirectUrl:'http://www.gfycat.com/' + gfyResponse.gfyname}; //redirect the user to the newly-generated gfycat video
 			}
-			chrome.history.deleteUrl({url:details.url}); //make sure the GIF page doesn't stay in history if gfycat is a bit slow to load
 		}
-	}
-	listenForNavigate();
-}
-function listenForNavigate(){
-	if(!chrome.webNavigation.onBeforeNavigate.hasListener(main)){
-		chrome.webNavigation.onBeforeNavigate.addListener(main);
+	}else{
+		return null;
 	}
 }
-chrome.runtime.onStartup.addListener(listenForNavigate);	//When Chrome opens and initializes itself, call the above function.
-chrome.runtime.onInstalled.addListener(listenForNavigate); //When the extension is installed or updated, call the above function.
+function listenForRequest(){
+	if(!chrome.webRequest.onBeforeRequest.hasListener(jiffyMain)){
+		chrome.webRequest.onBeforeRequest.addListener(jiffyMain, {urls:['https://*/*','http://*/*']}, ['blocking']);
+	}
+}
+chrome.runtime.onStartup.addListener(listenForRequest);	//When Chrome opens and initializes itself, call the above function.
+chrome.runtime.onInstalled.addListener(listenForRequest); //When the extension is installed or updated, call the above function.
